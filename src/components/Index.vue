ef26f5a0182866de465abe858308b2835eb10b92
孟凡旭 <mengfanxu@qimai.net>
2017-09-13 11:27:31 +0800

    搭建环境

---
+++ src/components/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="index">
+    <div class="title">{{msg}}</div>
+    <p class="content">反间谍法卡兰蒂斯积分</p>
+  </div>
+</template>
+
+<script>
+export default {
+   metaInfo: {
+    title: '首页', // set a title
+    meta: [{                 // set meta
+      name: 'keyWords',
+      content: 'My Example App'
+    },{
+      name: 'renderer',
+      content: 'webkit'
+    }],
+    link: [{                 // set link
+      rel: 'asstes',
+      href: 'https://assets-cdn.github.com/'
+    }]
+  },
+  data () {
+    return {
+      msg: 'ASO100'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  
+@import './../assets/css/common.scss';
+
+.index{
+  .title{
+    font-size: 24px;
+    color: #abcdef;
+  }
+  .content{
+    font-size: 26px;
+    @include dot(200px)
+  }
+}
+  
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+import MetaInfo from 'vue-meta-info'
+
+Vue.use(Vuex)
+Vue.use(iView)
+Vue.use(MetaInfo)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
